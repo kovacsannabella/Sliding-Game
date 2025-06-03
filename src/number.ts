e02b0698e3a46f193018bc2155616b333b1bea38
kovacsannabella <kovacs.annabella@students.jedlik.eu>
2025-06-03 19:43:18 +0200

    szabadHely és javítások

--- src/number.ts
+++ src/number.ts
@@ -1,11 +1,13 @@
-export class Mezo{
+import { formatDiagnostic } from "typescript";
+
+export class Mezo {
     ertek: number;
     x: number;
     y: number;
     mezok: Mezo[];
     meret: number;
 
-    constructor(ertek: number, mezok: Mezo[], meret: number){
+    constructor(ertek: number, mezok: Mezo[], meret: number) {
         this.ertek = ertek;
         this.x = 0;
         this.y = 0;
@@ -14,7 +16,7 @@ export class Mezo{
 
     }
 
-    szamotMozgat(x: number, y: number){
+    szamotMozgat(x: number, y: number) {
         const regiHely = this.y * this.meret + this.x;
         const regiDiv = document.querySelectorAll('.grid-item')[regiHely];
         if (regiDiv) regiDiv.innerHTML = '';
@@ -25,16 +27,16 @@ export class Mezo{
         const ujHely = y * this.meret + x;
         const ujDiv = document.querySelectorAll('.grid-item')[ujHely];
         if (ujDiv) {
-            const div = this.Megjelenit();
+            const div = this.megjelenit();
             ujDiv.appendChild(div);
         }
 
     }
 
-    Megjelenit(): HTMLDivElement {
-        const div = document.Megjelenit('div');
-        div.osztalyNev = 'number';
-        div.szoveg = this.ertek.toString();
+    megjelenit(): HTMLDivElement {
+        const div = document.createElement('div');
+        div.className = 'number';
+        div.innerText = this.ertek.toString();
 
         div.onclick = () => {
             const ures = this.szabadHely();
@@ -43,6 +45,34 @@ export class Mezo{
             }
         };
         return div;
+    }
+
+    szabadHely(): { x: number, y: number } | null {
+        const iranyok = [
+            { ix: -1, iy: 0 }, // bal
+            { ix: 1, iy: 0 }, // jobb
+            { ix: 0, iy: -1 }, // fel
+            { ix: 0, iy: 1 } // le
+        ];
+
+        for (let irany of iranyok) {
+            const ujX = this.x + irany.ix;
+            const ujY = this.y + irany.iy;
+
+            if (ujX >= 0 && ujX < this.meret && ujY >= 0 && ujY < this.meret) {
+                let foglalt = false;
+                for (let e of this.mezok) {
+                    if (e.x === ujX && e.y === ujY) {
+                        foglalt = true;
+                        break;
+                    }
+                }
+                if (!foglalt) {
+                    return { x: ujX, y: ujY };
+                }
+            }
+        }
+        return null;
     }
 
 }
