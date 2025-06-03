ab77e89a16b9dba826fa3c0fcaf6d2bea06857d0
baracskaidora <baracskai.dora@students.jedlik.eu>
2025-06-03 20:48:43 +0200

    már öszze keveri a számokat

--- dist/app.js
+++ dist/app.js
@@ -4,15 +4,26 @@ const mezok = [];
 const container = document.createElement("div");
 container.className = "grid-container";
 document.body.appendChild(container);
+const ertekek = [];
+for (let i = 1; i < meret * meret; i++) {
+    ertekek.push(i);
+}
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+shuffle(ertekek);
 for (let i = 0; i < meret * meret; i++) {
     const div = document.createElement("div");
     div.className = "grid-item";
     container.appendChild(div);
 }
-for (let i = 1; i < meret * meret; i++) {
-    const mezo = new Mezo(i, mezok, meret);
-    mezo.x = (i - 1) % meret;
-    mezo.y = Math.floor((i - 1) / meret);
+for (let i = 0; i < ertekek.length; i++) {
+    const mezo = new Mezo(ertekek[i], mezok, meret);
+    mezo.x = i % meret;
+    mezo.y = Math.floor(i / meret);
     mezok.push(mezo);
     const index = mezo.y * meret + mezo.x;
     const gridItem = container.children[index];
