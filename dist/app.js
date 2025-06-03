8dea933688f3137b9956d21b5718ce6bea165dc9
baracskaidora <baracskai.dora@students.jedlik.eu>
2025-06-03 20:32:46 +0200

    müködik az app.ts, fura de müködik

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var number_1 = require("./number");
-var meret = 10;
-var mezok = [];
-function init() {
-    var grid = document.createElement('div');
-    grid.className = 'grid-container';
-    for (var i = 0; i < meret * meret; i++) {
-        var cell = document.createElement('div');
-        cell.className = 'grid-item';
-        grid.appendChild(cell);
-    }
-    document.body.appendChild(grid);
-    for (var i = 1; i < meret * meret; i++) {
-        var mezo = new number_1.Mezo(i, mezok, meret);
-        mezo.x = (i - 1) % meret;
-        mezo.y = Math.floor((i - 1) / meret);
-        mezok.push(mezo);
-        var div = mezo.megjelenit();
-        var index = mezo.y * meret + mezo.x;
-        var hely = document.querySelectorAll('.grid-item')[index];
-        if (hely) {
-            hely.appendChild(div);
-        }
-    }
+import { Mezo } from "./number.js";
+const meret = 10;
+const mezok = [];
+const container = document.createElement("div");
+container.className = "grid-container";
+document.body.appendChild(container);
+for (let i = 0; i < meret * meret; i++) {
+    const div = document.createElement("div");
+    div.className = "grid-item";
+    container.appendChild(div);
 }
-init();
+for (let i = 1; i < meret * meret; i++) {
+    const mezo = new Mezo(i, mezok, meret);
+    mezo.x = (i - 1) % meret;
+    mezo.y = Math.floor((i - 1) / meret);
+    mezok.push(mezo);
+    const index = mezo.y * meret + mezo.x;
+    const gridItem = container.children[index];
+    gridItem.appendChild(mezo.megjelenit());
+}
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,25 @@
-import { Mezo } from './number';
+import { Mezo } from "./number.js";
 
 const meret = 10;
 const mezok: Mezo[] = [];
 
-function init() {
-    const grid = document.createElement('div');
-    grid.className = 'grid-container';
+const container = document.createElement("div");
+container.className = "grid-container";
+document.body.appendChild(container);
 
-    for (let i = 0; i < meret * meret; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'grid-item';
-        grid.appendChild(cell);
-    }
-
-    document.body.appendChild(grid);
-
-    for (let i = 1; i < meret * meret; i++) {
-        const mezo = new Mezo(i, mezok, meret);
-        mezo.x = (i - 1) % meret;
-        mezo.y = Math.floor((i - 1) / meret);
-        mezok.push(mezo);
-
-        const div = mezo.megjelenit();
-        const index = mezo.y * meret + mezo.x;
-        const hely = document.querySelectorAll('.grid-item')[index];
-        if (hely) {
-            hely.appendChild(div);
-        }
-    }
+for (let i = 0; i < meret * meret; i++) {
+    const div = document.createElement("div");
+    div.className = "grid-item";
+    container.appendChild(div);
 }
 
-init();
+for (let i = 1; i < meret * meret; i++) {
+    const mezo = new Mezo(i, mezok, meret);
+    mezo.x = (i - 1) % meret;
+    mezo.y = Math.floor((i - 1) / meret);
+    mezok.push(mezo);
+
+    const index = mezo.y * meret + mezo.x;
+    const gridItem = container.children[index] as HTMLElement;
+    gridItem.appendChild(mezo.megjelenit());
+}
