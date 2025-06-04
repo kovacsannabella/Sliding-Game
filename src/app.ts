116f7e4d66e8273f19f0deb4c71bb5f3900573e1
baracskaidora <baracskai.dora@students.jedlik.eu>
2025-06-04 13:33:51 +0200

    random üres mező

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,20 @@ const container = document.createElement("div");
 container.className = "grid-container";
 document.body.appendChild(container);
 
-const ertekek = [];
+const ertekek: (number | null)[] = [];
 for (let i = 1; i < meret * meret; i++) {
     ertekek.push(i);
 }
+ertekek.push(null);
 
-function shuffle(array: number[]) {
+shuffle(ertekek);
+
+function shuffle(array: (number | null)[]) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-
-shuffle(ertekek);
 
 for (let i = 0; i < meret * meret; i++) {
     const div = document.createElement("div");
@@ -27,12 +28,19 @@ for (let i = 0; i < meret * meret; i++) {
 }
 
 for (let i = 0; i < ertekek.length; i++) {
-    const mezo = new Mezo(ertekek[i], mezok, meret);
-    mezo.x = i % meret;
-    mezo.y = Math.floor(i / meret);
-    mezok.push(mezo);
-
-    const index = mezo.y * meret + mezo.x;
+    const ertek = ertekek[i];
+    const x = i % meret;
+    const y = Math.floor(i / meret);
+    const index = y * meret + x;
     const gridItem = container.children[index] as HTMLElement;
-    gridItem.appendChild(mezo.megjelenit());
+
+    if (ertek !== null) {
+        const mezo = new Mezo(ertek, mezok, meret);
+        mezo.x = x;
+        mezo.y = y;
+        mezok.push(mezo);
+        gridItem.appendChild(mezo.megjelenit());
+    } else {
+
+    }
 }
